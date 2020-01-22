fa6de8229a081d834b0d718fab4ac79fc6ad0cd1
you4rin <artichoke42@naver.com>
2020-01-23 00:46:16 +0900

    test

---
+++ Dodge/Assets/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+	private Rigidbody bulletRigidbody;
+	public float speed = 8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+		bulletRigidbody = GetComponent<Rigidbody>();
+		bulletRigidbody.velocity = transform.forward * speed;
+		Destroy(gameObject, 3f);
+    }
+	private void OnTriggerEnter(Collider other) {
+		if ( other.tag == "Player" ) {
+			PlayerController playerController = other.GetComponent<PlayerController>();
+
+			if ( playerController != null ) {
+				playerController.Die();
+			}
+		}
+	}
+}
