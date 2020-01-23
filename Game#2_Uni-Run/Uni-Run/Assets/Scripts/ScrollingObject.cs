3a520dbeb2f8b0faa6416abdc4207c6437dd71f9
you4rin <artichoke42@naver.com>
2020-01-24 05:28:57 +0900

    Complete Uni-Run

--- Game#2_Uni-Run/Uni-Run/Assets/Scripts/ScrollingObject.cs
+++ Game#2_Uni-Run/Uni-Run/Assets/Scripts/ScrollingObject.cs
@@ -5,6 +5,9 @@ public class ScrollingObject : MonoBehaviour {
     public float speed = 10f; // 이동 속도
 
     private void Update() {
-        // 게임 오브젝트를 왼쪽으로 일정 속도로 평행 이동하는 처리
+		// 게임 오브젝트를 왼쪽으로 일정 속도로 평행 이동하는 처리
+		if ( !GameManager.instance.isGameover ) {
+			transform.Translate(Vector3.left * speed * Time.deltaTime);
+		}
     }
 }
